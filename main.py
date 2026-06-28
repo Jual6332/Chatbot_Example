6116a7bb03e76d69d20e541d25e464a3044317aa
jual6332@colorado.edu <jual6332@colorado.edu>
2026-06-27 22:47:50 -0400

    Added new trainer for training chatbot

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import JsonFileTrainer
 
 def main():
 	print("Main function")
 
 	chatbot = ChatBot("Animals University")
+
+	'''
+	trainer = ListTrainer(chatbot)
+	# The .train() function injects entries into your database to build upon 
+	# the graph structure that Chatterbot uses to choose possible replies.
+	trainer.train([
+		"Hi",
+		"Welcome, friend"
+	])
+	trainer.train([
+		"Are you a plant?",
+		"No, I am an owl! Your guide to Animals University - in computer form!"
+	])'''
+
+	trainer = JsonFileTrainer(
+		chatbot,
+		field_map={
+			'text':'text',
+			'in_response_to':'in_response_to',
+			'persona':'persona',
+			'conversation':'conversation'
+		}
+	)
+
+	trainer.train('./data/training_data.json')
 
 	exit_conditions = ("q","quit","exit")
 	while True:
